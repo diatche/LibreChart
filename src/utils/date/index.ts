594283ebe28dc0dff8bb532c845a7e9fca04c876
Pavel Diatchenko <diatche@gmail.com>
2020-11-27 17:02:24 +1300

    Fixed export.

--- src/utils/date/index.ts
+++ src/utils/date/index.ts
@@ -1,7 +1,7 @@
 export * from './dateBase';
 export * from './duration';
 
-export { default as dateScale } from './DateScale';
+export { default as DateScale } from './DateScale';
 export * from './DateScale';
 
 export { default as DateAxis } from './DateAxis';
