8baa8e8ba6730a3981ad2e3ca8bf645a31939118
Pavel Diatchenko <diatche@gmail.com>
2020-12-03 10:13:34 +1300

    Fixed date tick style bugs.

--- src/utils/date/dateStyle.ts
+++ src/utils/date/dateStyle.ts
@@ -11,39 +11,47 @@ import {
 import { getUniformMs } from "./duration";
 
 export const getTickStyles = (duration: moment.Duration, style: IAxisStyle): DateUnitMapping<TextStyle> => {
-    // Find unit which has a normal loabel style
+    // Find unit which has a regular label style
     let uniformMs = getUniformMs(duration);
-    let normalIndex = 0;
+    let regularIndex = 0;
+    let otherRegularIndex = -1;
     for (let i = 0; i < kDateUnitsLength; i++) {
         let dateUnit = kDateUnitsAsc[i];
         let unitDensity = uniformMs / kDateUnitUniformMs[dateUnit];
-        // console.debug(`${dateUnit} unitDensity: ` + unitDensity);
-        if (dateUnit === 'month') {
-            if (unitDensity < 0.4) {
-                break;   
+        if (unitDensity < 1 && dateUnit === 'month') {
+            // Not full month, mark days as regular
+            regularIndex = i - 1;
+            let days = duration.as('days');
+            if (days > 10) {
+                // Too many days, mark month as regular as well
+                otherRegularIndex = i;
             }
+            break;
         }
         if (unitDensity < 0.25) {
             break;
         }
-        normalIndex = i;
+        regularIndex = i;
     }
 
     return mapDateUnits((dateUnit: DateUnit, index: number): TextStyle => {
-        if (index > normalIndex) {
+        if (index === regularIndex || index === otherRegularIndex) {
+            // Regular style
+            return {
+                color: style.labelColor,
+                fontWeight: style.labelFontWeight,
+            };
+        } else if (index > regularIndex) {
+            // Strong style
             return {
                 color: style.majorLabelColor,
                 fontWeight: style.majorLabelFontWeight,
             };
-        } else if (index < normalIndex) {
+        } else {
+            // Subtle style
             return {
                 color: style.minorLabelColor,
                 fontWeight: style.minorLabelFontWeight,
-            };
-        } else {
-            return {
-                color: style.labelColor,
-                fontWeight: style.labelFontWeight,
             };
         }
     });
--- tests/utils/date/dateStyle.test.ts
+++ tests/utils/date/dateStyle.test.ts
@@ -8,6 +8,22 @@ import {
 import {
     getTickStyles,
 } from '../../../src/utils/date/dateStyle';
+import { TextStyle } from 'react-native';
+
+const strongStyle: TextStyle = {
+    color: 'red',
+    fontWeight: '300',
+};
+
+const regularStyle: TextStyle = {
+    color: 'green',
+    fontWeight: '200',
+};
+
+const subtleStyle: TextStyle = {
+    color: 'blue',
+    fontWeight: '100',
+};
 
 describe('dateStyle', () => {
 
@@ -15,30 +31,289 @@ describe('dateStyle', () => {
 
         const axisStyle: IAxisStyle = {
             ...kAxisStyleLightDefaults,
-            majorLabelColor: 'red',
-            labelColor: 'green',
-            minorLabelColor: 'blue',
+            majorLabelColor: strongStyle.color!,
+            labelColor: regularStyle.color!,
+            minorLabelColor: subtleStyle.color!,
 
-            majorLabelFontWeight: '300',
-            labelFontWeight: '200',
-            minorLabelFontWeight: '100',
+            majorLabelFontWeight: strongStyle.fontWeight!,
+            labelFontWeight: regularStyle.fontWeight!,
+            minorLabelFontWeight: subtleStyle.fontWeight!,
         };
 
-        it('should style days normally with 1 day scale', () => {
-            expect(getTickStyles(moment.duration(1, 'month'), axisStyle)).toEqual({
-                color: 'red',
-                labelFontWeight: '300',
-            });
+        it('should style correctly with 100 millisecond scale', () => {
+            let styles = getTickStyles(moment.duration(100, 'millisecond'), axisStyle);
 
-            expect(getTickStyles(moment.duration(1, 'day'), axisStyle)).toEqual({
-                color: 'green',
-                labelFontWeight: '200',
-            });
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(strongStyle);
+            expect(styles['second']).toEqual(strongStyle);
+            expect(styles['millisecond']).toEqual(regularStyle);
+        });
 
-            expect(getTickStyles(moment.duration(1, 'hour'), axisStyle)).toEqual({
-                color: 'blue',
-                labelFontWeight: '100',
-            });
+        it('should style correctly with 200 millisecond scale', () => {
+            let styles = getTickStyles(moment.duration(200, 'millisecond'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(strongStyle);
+            expect(styles['second']).toEqual(strongStyle);
+            expect(styles['millisecond']).toEqual(regularStyle);
+        });
+
+        it('should style correctly with 500 millisecond scale', () => {
+            let styles = getTickStyles(moment.duration(500, 'millisecond'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(strongStyle);
+            expect(styles['second']).toEqual(regularStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 second scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'second'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(strongStyle);
+            expect(styles['second']).toEqual(regularStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 10 second scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'second'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(strongStyle);
+            expect(styles['second']).toEqual(regularStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 20 second scale', () => {
+            let styles = getTickStyles(moment.duration(20, 'second'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(regularStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 30 second scale', () => {
+            let styles = getTickStyles(moment.duration(30, 'second'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(regularStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 minute scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'minute'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(regularStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 10 minute scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'minute'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(strongStyle);
+            expect(styles['minute']).toEqual(regularStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 20 minute scale', () => {
+            let styles = getTickStyles(moment.duration(20, 'minute'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(regularStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 30 minute scale', () => {
+            let styles = getTickStyles(moment.duration(30, 'minute'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(regularStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 hour scale', () => {
+            let styles = getTickStyles(moment.duration(1, 'hour'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(regularStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 3 hour scale', () => {
+            let styles = getTickStyles(moment.duration(3, 'hour'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(strongStyle);
+            expect(styles['hour']).toEqual(regularStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 12 hour scale', () => {
+            let styles = getTickStyles(moment.duration(12, 'hour'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(regularStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 day scale', () => {
+            let styles = getTickStyles(moment.duration(1, 'day'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(regularStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 2 day scale', () => {
+            let styles = getTickStyles(moment.duration(1, 'day'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(regularStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 10 day scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'day'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(strongStyle);
+            expect(styles['day']).toEqual(regularStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 20 day scale', () => {
+            let styles = getTickStyles(moment.duration(20, 'day'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(regularStyle);
+            expect(styles['day']).toEqual(regularStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 month scale', () => {
+            let styles = getTickStyles(moment.duration(1, 'month'), axisStyle);
+
+            expect(styles['year']).toEqual(strongStyle);
+            expect(styles['month']).toEqual(regularStyle);
+            expect(styles['day']).toEqual(subtleStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 3 month scale', () => {
+            let styles = getTickStyles(moment.duration(3, 'month'), axisStyle);
+
+            expect(styles['year']).toEqual(regularStyle);
+            expect(styles['month']).toEqual(subtleStyle);
+            expect(styles['day']).toEqual(subtleStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 6 month scale', () => {
+            let styles = getTickStyles(moment.duration(6, 'month'), axisStyle);
+
+            expect(styles['year']).toEqual(regularStyle);
+            expect(styles['month']).toEqual(subtleStyle);
+            expect(styles['day']).toEqual(subtleStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 1 year scale', () => {
+            let styles = getTickStyles(moment.duration(1, 'year'), axisStyle);
+
+            expect(styles['year']).toEqual(regularStyle);
+            expect(styles['month']).toEqual(subtleStyle);
+            expect(styles['day']).toEqual(subtleStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
+        });
+
+        it('should style correctly with 10 year scale', () => {
+            let styles = getTickStyles(moment.duration(10, 'year'), axisStyle);
+
+            expect(styles['year']).toEqual(regularStyle);
+            expect(styles['month']).toEqual(subtleStyle);
+            expect(styles['day']).toEqual(subtleStyle);
+            expect(styles['hour']).toEqual(subtleStyle);
+            expect(styles['minute']).toEqual(subtleStyle);
+            expect(styles['second']).toEqual(subtleStyle);
+            expect(styles['millisecond']).toEqual(subtleStyle);
         });
     });
 });
